ade34139d2d9864cf4a4b729b4c2bc7d880266f5
Florian Sey <florian.sey@gmail.com>
2019-02-08 12:08:58 +0100

    Add discrepancies

---
+++ src/main/java/lu/cerberians/sollist/discrepancy/DiscrepanciesDto.java
@@ -0,0 +1,13 @@
+package lu.cerberians.sollist.discrepancy;
+
+import lombok.Data;
+import lu.cerberians.sollist.entities.Loop;
+
+import java.util.List;
+
+@Data
+public class DiscrepanciesDto {
+
+    private List<Loop> discrepancies;
+
+}
