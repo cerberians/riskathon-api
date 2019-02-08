deba875c7ad25f7e4b0efda6e0e3dc2b84abe3c7
Florian Sey <florian.sey@gmail.com>
2019-02-08 07:28:43 +0100

    Fix create asset function

--- src/main/java/lu/cerberians/sollist/entities/AssetFunction.java
+++ src/main/java/lu/cerberians/sollist/entities/AssetFunction.java
@@ -2,9 +2,11 @@ package lu.cerberians.sollist.entities;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@ToString
 public class AssetFunction extends Entity {
     public AssetFunction() {
     }
